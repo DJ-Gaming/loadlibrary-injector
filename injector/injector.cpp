7e2c7f89b93c6bcfee7e23d468a6fa016119754f
The Doge <55068202+DJ-Gaming@users.noreply.github.com>
2021-03-03 13:10:40 -0800

    Now Restores Bytes

--- injector/injector.cpp
+++ injector/injector.cpp
@@ -4,18 +4,21 @@ const char* dll = "csgo-cheat.dll"; // replace with your dll
 
 int main()
 {
+	char restoreBytes[5];
 	int Pid = ProcessID("csgo.exe");
 	HANDLE Proc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, Pid);
 
-	//credit: Daniel Krupiñski
+	//credit: Daniel KrupiÃ±ski
 	LPVOID ntOpenFile = GetProcAddress(LoadLibraryW(L"ntdll"), "NtOpenFile");
 	if (ntOpenFile) {
 		char originalBytes[5];
 		memcpy(originalBytes, ntOpenFile, 5);
+		ReadProcessMemory(Proc, ntOpenFile, restoreBytes, 5, NULL);
 		WriteProcessMemory(Proc, ntOpenFile, originalBytes, 5, NULL);
 	}
 
 	inject(Pid, dll);
+	WriteProcessMemory(Proc, ntOpenFile, restoreBytes, 0, NULL);
 	std::cout << "Successfully Injected " << dll << "!";
 	Sleep(3000);
 	exit(0);
